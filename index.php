0edaf3134d048c2cc05916f64a57599b7dfda690
Axel Cureño <axelcureno@gmail.com>
2015-06-16 13:59:33 -0500

    analytics arreglo

--- index.php
+++ index.php
@@ -256,16 +256,6 @@ if (isset($_GET['cribsearch']) && $_GET['cribsearch'] != '') {
         <script src="<?php echo URL ?>js/plugins.js"></script>
         <script src="<?php echo URL ?>js/main.js"></script>
         <script src="<?php echo URL ?>js/jquery.ui.touch-punch.min.js"></script>
-        <script>
-          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-          })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-          ga('create', 'UA-47439231-8', 'auto');
-          ga('send', 'pageview');
-
-        </script>
         <!--<script type="text/javascript" src="js/materialize.min.js"></script>-->
         <script type="text/javascript">
             $('.bxslider').bxSlider({
@@ -408,5 +398,15 @@ if (isset($_GET['cribsearch']) && $_GET['cribsearch'] != '') {
         });
         </script>
         <script>$('#slider-precio-cribsearch').slider();</script>
+                <script>
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+          ga('create', 'UA-47439231-8', 'auto');
+          ga('send', 'pageview');
+
+        </script>
     </body>
 </html>
