a91142750fccf1430bc6f5cffffeb0df3368bf9a
Axel Cureño <axelcureno@gmail.com>
2015-06-26 22:16:52 -0500

    Se agrego el precio al webservice de los markers

--- markerswebservice.php
+++ markerswebservice.php
@@ -1,6 +1,6 @@
 <?php
 include("functions.php");
-$sth = mysqli_query($con, "SELECT titulocrib,urlcrib,categoriacrib,latitudcrib,longitudcrib, imagenprincipalcrib FROM cribs");
+$sth = mysqli_query($con, "SELECT titulocrib,urlcrib,categoriacrib,latitudcrib,longitudcrib,imagenprincipalcrib, preciocrib FROM cribs");
 $rows = array();
 while($r = mysqli_fetch_assoc($sth)) {
     $rows[] = $r;
